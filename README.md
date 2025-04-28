3fe4b27e773d35cc7822a11af9047318f3c320ce
Ivan Dovhal <limetrack@gmail.com>
2025-04-28 12:11:09 +0300

    task

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# goit-node-cli
+# Contact Management CLI App
+
+This is a simple command-line application for managing contacts. It allows you to list, add, remove, and get contacts using a `contacts.json` file.
+
+### How to use
+
+Clone or download the repository to your local machine.
+`npm install`
+
+### Commands
+
+1. List All Contacts
+   `node index.js -a list`
+2. Get a Contact by ID
+   `node index.js -a get -i <id>`
+3. Add a New Contact
+   `node index.js -a add -n "Name" -e "email@example.com" -p "123-456-7890"`
+4. Remove a Contact by ID
+   `node index.js -a remove -i <id>`
---
+++ contacts.js
@@ -0,0 +1,53 @@
+import fs from "fs/promises";
+import path from "path";
+
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+const contactsPath = path.join(__dirname, 'db', 'contacts.json');
+
+async function listContacts() {
+  const data = await fs.readFile(contactsPath, 'utf8');
+
+  return JSON.parse(data);
+}
+
+async function getContactById(contactId) {
+	const contacts = await listContacts();
+	const contact = contacts.find(contact => contact.id === contactId);
+
+  return contact || null;
+}
+
+async function addContact(name, email, phone) {
+  const contacts = await listContacts();
+  const newContact = {
+    id: `${new Date().valueOf()}`,
+    name,
+    email,
+    phone
+  };
+  contacts.push(newContact);
+
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+
+  return newContact;
+}
+
+async function removeContact(contactId) {
+  const contacts = await listContacts();
+  const index = contacts.findIndex(contact => contact.id === contactId);
+
+  if (index === -1) return null;
+
+  const [removedContact] = contacts.splice(index, 1);
+
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+
+  return removedContact;
+}
+
+export {
+	listContacts,
+	getContactById,
+	removeContact,
+	addContact,
+};
